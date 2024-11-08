561791a64d712f331152dc5169b5fb7229fd382f
Celis620 <mauro.celis620@gmail.com>
2024-11-08 18:48:08 -0500

    Vista de añadir terreno funcional

--- src/pages/AnadirTerreno.jsx
+++ src/pages/AnadirTerreno.jsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { TextField, Button, Container, Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import { useNavigate } from "react-router-dom";  // Importar el hook de navegación de React Router
+import React, { useEffect, useState } from "react";
+import { TextField, Button, Typography, CircularProgress } from "@mui/material";
+import Box from '@mui/material/Box';
+import Swal from 'sweetalert2'; // Importar SweetAlert2
 
-const CrearTerreno = () => {
-    const navigate = useNavigate(); // Hook para navegación
-    const [formData, setFormData] = useState({
+const AñadirTerreno = () => {
+    const [perfil, setPerfil] = useState(null);
+    const [loadingPerfil, setLoadingPerfil] = useState(true);
+    const [terreno, setTerreno] = useState({
         idTerreno: "",
         ubicacion: {
             pais: "",
@@ -16,150 +17,200 @@ const CrearTerreno = () => {
         tamano: "",
         tipoPasto: "",
         precio: "",
+        historialAlquileres: [],
         imagenes: []
     });
+    const [loading, setLoading] = useState(false); // Estado de carga para la solicitud
 
-    // Verificación del authToken al cargar la página
     useEffect(() => {
         const authToken = localStorage.getItem("authToken");
-        if (!authToken) {
-            alert("Please log in first");
-            navigate("/login");  // Redirigir a la página de inicio de sesión
-        }
-    }, [navigate]);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value
-        }));
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const authToken = localStorage.getItem("authToken");
         if (!authToken) {
-            alert("Please log in first");
+            alert("Por favor, inicia sesión primero.");
+            setLoadingPerfil(false);
             return;
         }
 
-        const response = await fetch('http://localhost:8070/terreno/ingresar_terreno', {
-            method: 'POST',
+        fetch('http://localhost:8070/usuario/perfil', {
+            method: 'GET',
             headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${authToken}` 
-            },
-            body: JSON.stringify(formData)
+                'Authorization': `Bearer ${authToken}`,
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            setPerfil(data[0]);
+            setLoadingPerfil(false);
+        })
+        .catch((error) => {
+            console.error('Error al obtener el perfil del usuario:', error);
+            setLoadingPerfil(false);
         });
+    }, []);
 
-        if (response.ok) {
-            console.log("Terreno ingresado correctamente");
+    const handleChangeTerreno = (e) => {
+        const { name, value } = e.target;
+
+        if (name.includes("ubicacion")) {
+            const field = name.split(".")[1];
+            setTerreno((prevTerreno) => ({
+                ...prevTerreno,
+                ubicacion: {
+                    ...prevTerreno.ubicacion,
+                    [field]: value
+                }
+            }));
         } else {
-            console.log("Error al ingresar terreno");
+            setTerreno({
+                ...terreno,
+                [name]: value
+            });
         }
     };
 
+    const handleAñadirTerreno = () => {
+        if (!perfil) {
+            alert("No se pudo obtener el perfil del usuario.");
+            return;
+        }
+
+        const authToken = localStorage.getItem("authToken");
+
+        const nuevoTerreno = {
+            ...terreno,
+            emailUsuario: perfil.email
+        };
+
+        setLoading(true); // Activar el estado de carga
+
+        fetch('http://localhost:8070/terreno/ingresar_terreno', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${authToken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(nuevoTerreno)
+        })
+        .then(response => response.json())
+        .then(data => {
+            setLoading(false); // Desactivar el estado de carga
+            Swal.fire({
+                icon: 'success',
+                title: 'Terreno añadido exitosamente',
+                text: 'El terreno se ha añadido correctamente.',
+                confirmButtonText: 'Aceptar'
+            });
+        })
+        .catch(error => {
+            setLoading(false); // Desactivar el estado de carga
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Ocurrió un error al añadir el terreno.',
+                confirmButtonText: 'Aceptar'
+            });
+        });
+    };
+
     return (
-        <Container maxWidth="sm" className="mt-8">
-            <Box className="bg-white p-8 rounded-lg shadow-lg">
-                <Typography variant="h5" className="text-center mb-6">
-                    Ingresar Terreno
+        <div className="flex justify-center items-center h-screen">
+            <Box
+                component="form"
+                sx={{
+                    '& .MuiTextField-root': { m: 1, width: '35ch' },
+                }}
+                className="shadow-lg p-10 bg-white rounded-lg"
+                noValidate
+                autoComplete="off"
+            >
+                <Typography variant="h5" component="div" gutterBottom>
+                    Añadir Terreno
                 </Typography>
-                <form onSubmit={handleSubmit}>
-                    <TextField
-                        label="ID Terreno"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        name="idTerreno"
-                        value={formData.idTerreno}
-                        onChange={handleChange}
-                        required
-                    />
-                    <TextField
-                        label="Pais"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        name="ubicacion.pais"
-                        value={formData.ubicacion.pais}
-                        onChange={handleChange}
-                        required
-                    />
-                    <TextField
-                        label="Departamento"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        name="ubicacion.departamento"
-                        value={formData.ubicacion.departamento}
-                        onChange={handleChange}
-                        required
-                    />
-                    <TextField
-                        label="Ciudad"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        name="ubicacion.ciudad"
-                        value={formData.ubicacion.ciudad}
-                        onChange={handleChange}
-                        required
-                    />
-                    <TextField
-                        label="Dirección"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        name="ubicacion.direccion"
-                        value={formData.ubicacion.direccion}
-                        onChange={handleChange}
-                        required
-                    />
-                    <TextField
-                        label="Tamaño"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        name="tamano"
-                        value={formData.tamano}
-                        onChange={handleChange}
-                        required
-                    />
-                    <TextField
-                        label="Tipo de Pasto"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        name="tipoPasto"
-                        value={formData.tipoPasto}
-                        onChange={handleChange}
-                        required
-                    />
-                    <TextField
-                        label="Precio"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        name="precio"
-                        value={formData.precio}
-                        onChange={handleChange}
-                        required
-                    />
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        type="submit"
-                        fullWidth
-                        className="mt-6"
-                    >
-                        Ingresar Terreno
-                    </Button>
-                </form>
+
+                {loadingPerfil ? (
+                    <CircularProgress />
+                ) : perfil ? (
+                    <>
+                        <Typography variant="body1" gutterBottom>
+                            Usuario: {perfil.email}
+                        </Typography>
+
+                        <TextField
+                            required
+                            label="ID del Terreno"
+                            name="idTerreno"
+                            value={terreno.idTerreno}
+                            onChange={handleChangeTerreno}
+                        />
+                        <TextField
+                            required
+                            label="País"
+                            name="ubicacion.pais"
+                            value={terreno.ubicacion.pais}
+                            onChange={handleChangeTerreno}
+                        />
+                        <TextField
+                            required
+                            label="Departamento"
+                            name="ubicacion.departamento"
+                            value={terreno.ubicacion.departamento}
+                            onChange={handleChangeTerreno}
+                        />
+                        <TextField
+                            required
+                            label="Ciudad"
+                            name="ubicacion.ciudad"
+                            value={terreno.ubicacion.ciudad}
+                            onChange={handleChangeTerreno}
+                        />
+                        <TextField
+                            required
+                            label="Dirección"
+                            name="ubicacion.direccion"
+                            value={terreno.ubicacion.direccion}
+                            onChange={handleChangeTerreno}
+                        />
+                        <TextField
+                            required
+                            label="Tamaño"
+                            name="tamano"
+                            value={terreno.tamano}
+                            onChange={handleChangeTerreno}
+                        />
+                        <TextField
+                            required
+                            label="Tipo de Pasto"
+                            name="tipoPasto"
+                            value={terreno.tipoPasto}
+                            onChange={handleChangeTerreno}
+                        />
+                        <TextField
+                            required
+                            label="Precio"
+                            name="precio"
+                            value={terreno.precio}
+                            onChange={handleChangeTerreno}
+                        />
+
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            className="mt-4"
+                            onClick={handleAñadirTerreno}
+                            disabled={loading} // Deshabilitar botón mientras se procesa la solicitud
+                        >
+                            {loading ? <CircularProgress size={24} /> : 'Añadir Terreno'}
+                        </Button>
+                    </>
+                ) : (
+                    <Typography variant="body1" color="error">
+                        Error al cargar el perfil del usuario.
+                    </Typography>
+                )}
             </Box>
-        </Container>
+        </div>
     );
 };
 
-export default CrearTerreno;
+export default AñadirTerreno;
